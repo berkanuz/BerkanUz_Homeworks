e8c8861aa7fceb48594d04e9a20457bd70636f01
Berkan <berkanuz@hotmail.com>
2021-10-23 03:21:30 +0300

    delete unnecessary lines.

--- 1-ToDo/index.js
+++ 1-ToDo/index.js
@@ -2,9 +2,10 @@ const button = document.querySelector("#button-save");
 const button1 = document.querySelector("#button-delete");
 const text = document.querySelector("#text-input");
 const ulList = document.querySelector("#ul-list");
+const bodyContent = document.querySelector("#body-content")
 const localStorageKey = "todoList";
 let listStr = "";
-//const bootstrap = require('bootstrap');
+var modalItem = null;
 
 renderOnScreen();
 
@@ -21,22 +22,15 @@ function addClickButton() {
         };
         writeITemToLocalStorage(item);
         renderOnScreen();
-        //alert("SAVE IS SUCCESS")
         text.value = '';
     }
     else {
-        //alert("TEXT CAN NOT BE NULL")
+        alert("Text can not be null!")
     }
 }
 function generateId() {
     const localStorageValue = JSON.parse(localStorage.getItem(localStorageKey)) ?? []
-    var Id = 0;
-    localStorageValue.forEach(element => {
-        if (element.Id >= Id) {
-            Id = element.Id;
-        }
-    });
-    return Id + 1;
+    return localStorageValue.length + 1;
 
 }
 
@@ -52,6 +46,7 @@ function writeOnLocalStorage(localStorageValue) {
 }
 
 function renderOnScreen() {
+
     var renderString = "";
     var data = localStorage.getItem(localStorageKey);
     data = data ? JSON.parse(data) : [];
@@ -59,36 +54,35 @@ function renderOnScreen() {
         const item = data[i]
         const textValue = item.status == 0 ? "<p>" + item.text + "</p>" : "<del>" + item.text + "</del>"
         const rowHeader = "<th scope='row' class = 'row-header'> " + (i + 1) + "</th>"
-        const description = "<td>"+textValue+"</td>"
-        const deleteButton = "<td><button onclick='deleteItem(" + i + ")' class='btn btn-danger tableButton'> DELETE </button></td>";
-        const infoButton = "<td><button onclick='showItemInfo(" + i + ")'class='btn btn-info tableButton' > info </button></td>"
-        const statusButton = "<td><button onclick='setItemStatus(" + i + ")'class='btn btn-info tableButton' > Status </button></td>"
+        const description = "<td>" + textValue + "</td>"
+        const deleteButton = "<button onclick='deleteItem(" + i + ")' class='btn btn-danger tableButton'> Delete </button>";
+        const infoButton = "<button onclick='showItemInfo(" + i + ")'class='btn btn-info tableButton' > Info </button>"
 
-        const actionButtons = deleteButton + infoButton + statusButton;
+        const statusButton = "<button onclick='setItemStatus(" + i + ")'class='btn btn-" + (item.status == 0 ? "success" : "warning") + " tableButton' > " + (item.status == 0 ? "Done" : "Pending") + "</button>"
+
+        const actionButtons = "<td id='action-buttons-section'>" + deleteButton + infoButton + statusButton + "</td>";
         const row = "<tr>" + rowHeader + description + actionButtons + "</tr>";
-
-
-        //<li class="list-group-item list-group-item-success">First item</li>
 
         renderString += row;
     }
     ulList.innerHTML = renderString;
+
 }
 function deleteItem(itemIndex) {
     let localStorageValue = JSON.parse(localStorage.getItem(localStorageKey));
     localStorageValue.splice(itemIndex, 1);
     writeOnLocalStorage(localStorageValue);
-    renderOnScreen();
+    location.reload()
 }
 function showItemInfo(itemIndex) {
     let localStorageValue = JSON.parse(localStorage.getItem(localStorageKey));
-    console.log(JSON.stringify(localStorageValue[itemIndex]))
     let item = localStorageValue[itemIndex];
-    const alertString = "Message: " + item.text + " Creation date:" + new Date(item.createdDate).toLocaleDateString()
-    alert(alertString);
+    modalItem = item;
+    renderInfoModal()
+    $("#exampleModal").modal();
 }
 function deleteAllItem() {
-    let deleteAllTasks = confirm("Do you want to delete all list");
+    let deleteAllTasks = confirm("Do you want to delete all");
     if (deleteAllTasks) {
         localStorage.clear();
         location.reload()
@@ -100,11 +94,39 @@ function setItemStatus(itemIndex) {
     item.status = item.status == 1 ? 0 : 1
 
     writeOnLocalStorage(localStorageValue);
-    
+
     console.log(JSON.stringify(item))
+    location.reload()
+
+}
+function renderInfoModal() {
+    if (!modalItem)
+        return
+    let element = document.getElementById("exampleModal")
+    if (element)
+        element.parentNode.removeChild(element);
+
+    const modal = `<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <div><p>Todo task: `+ modalItem.text + `</p></div>
+        <div><p>Creation Date: `+ new Date(modalItem.createdDate).toLocaleDateString() + `</p></div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+
+    bodyContent.innerHTML += modal
+
     renderOnScreen();
-
-
-
-
 }
